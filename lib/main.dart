95491b0e1be0f57ad74fcca1e089bd3ab683b3cd
stereofantasy <vitolordvito@gmail.com>
2024-05-07 15:17:31 +0700

    k

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:untitled5/views/home_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter AR App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: const MyHomePage(),
+    );
+  }
+}
---
+++ lib/model/ar_objects.dart
@@ -0,0 +1,11 @@
+class ARObjects {
+  static const String fox = "assets/mig21/scene.gltf";
+  static const String figure =
+      "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/RiggedFigure/glTF-Binary/RiggedFigure.glb";
+  static const String shoe =
+      "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/MaterialsVariantsShoe/glTF-Binary/MaterialsVariantsShoe.glb";
+  static const String chair =
+      "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/SheenChair/glTF-Binary/SheenChair.glb";
+
+  static const String chicken = "assets/Chicken_01/Chicken_01.gltf";
+}
